5c8415e7b89c9db3da97315cda1cf192eb8857ca
Dixneuf Baptiste <baptistedixneuf@gmail.com>
2014-12-15 23:12:20 +0100

    Recherche ordonnée

--- app/controllers/RecherchesController.php
+++ app/controllers/RecherchesController.php
@@ -13,26 +13,31 @@ class RecherchesController extends BaseController{
 		    case 'film':
 		        $data=Film::select(array('id','titre as value'))
 				->where('titre','LIKE','%'.Input::get('InputRecherche').'%')
+				->orderBy('titre')
 				->get();
 		        break;
 		    case 'realisateur':
 		        $data=Realisateur::select(array('id','pre_nom_rea as value'))
 				->where('pre_nom_rea','LIKE','%'.Input::get('InputRecherche').'%')
+				->orderBy('pre_nom_rea')
 				->get();
 		        break;
 		    case 'distributeur':
 		       	$data=Distributeur::select(array('id','nom as value'))
 				->where('nom','LIKE','%'.Input::get('InputRecherche').'%')
+				->orderBy('nom')
 				->get();
 		        break;
 		    case 'genre':
 		       	$data=Genre::select(array('id','genre as value'))
 				->where('genre','LIKE','%'.Input::get('InputRecherche').'%')
+				->orderBy('genre')
 				->get();
 		        break; 
 		    case 'acteur':
 		       	$data=Acteur::select(array('id','pre_nom_acteur as value'))
 				->where('pre_nom_acteur','LIKE','%'.Input::get('InputRecherche').'%')
+				->orderBy('pre_nom_acteur')
 				->get();			
 		        break;    
 		    default:
