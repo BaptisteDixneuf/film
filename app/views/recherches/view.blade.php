6284bcc97f1fe495749953da0a2c9485c2ccdf49
Dixneuf Baptiste <baptistedixneuf@gmail.com>
2015-03-07 22:33:41 +0100

    bugfix aucun résultat recherche

--- app/views/recherches/view.blade.php
+++ app/views/recherches/view.blade.php
@@ -1,7 +1,8 @@
 <h1> Résultats de recherche </h1>
-
-@foreach($data as $d)
-
-	<li><a href="{{ URL::action($base_chemin,  $d->id)  }}">{{$d->value}}</a></li>
-	
-@endforeach
+@if(!empty($data->items))	
+	@foreach($data as $d)	
+		<li><a href="{{ URL::action($base_chemin,  $d->id)  }}">{{$d->value}}</a></li>	
+	@endforeach
+@else
+	Aucun Résultat
+@endif
